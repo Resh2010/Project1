35882a5b1e4b3ba40769c0754f036b4cc7ffddc6
Resh2010 <reshma.ramesh@hrblock.com>
2022-08-18 17:09:45 +0530

    created login page

--- LoginProject/src/app/app.module.ts
+++ LoginProject/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,10 @@ import { WelcomeComponent } from './welcome/welcome.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- LoginProject/src/app/login/login.component.ts
+++ LoginProject/src/app/login/login.component.ts
@@ -1,15 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup,FormBuilder, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public loginForm!:FormGroup
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder,private http:HttpClient,private router:Router) { }
 
   ngOnInit(): void {
+    this.loginForm=this.formBuilder.group({
+      name: ['', Validators.required],
+      password:['',Validators.required]
+    })
+  }
+  login(){
+    alert("username is "+this.loginForm.value.name);
+    alert("password is "+this.loginForm.value.password);
+
+
+
+   
+    // const user = (values: any) => {
+    //   {
+    //     // return a.email === this.loginForm.value.email &&
+    //     //   a.password === this.loginForm.value.password
+    //     return this.loginForm.value.email === "reshma" &&
+    //       this.loginForm.value.password === "12345"
+    //   };
+    // }
+    //   if (user){
+    //     alert("Login Success");
+    //     this.loginForm.reset();
+       
+    //   }else{
+    //     alert("User not found!!");
+    //   }
+    // }
   }
 
 }
